bfbb856bfc29753475dcefa19394c69674335f45
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-11-17 20:30:35 +0200

    feat: create signup form with tailwind code + animation

--- src/app/features/try/try.component.ts
+++ src/app/features/try/try.component.ts
@@ -1,9 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-try',
   templateUrl: './try.component.html',
-  styleUrl: './try.component.css'
+  styleUrl: './try.component.css',
+  animations: [
+    trigger('expandElement', [
+      state('void', style({
+        height: '0',
+        marginTop: '0',
+        opacity: '0',
+        transform: 'translateY(-20px)'
+      })),
+      transition(':enter', [
+        animate('0.5s ease-out', style({
+          height: '*',
+          marginTop: '*',
+          opacity: '1',
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
 export class TryComponent implements OnInit{
     ngOnInit(): void {
