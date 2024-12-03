8b9ef9f89789238b80da5acff8b262780c3d59c2
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-03 22:17:35 +0200

    refactor: update TryComponent to use new service and animations

--- src/app/features/try/try.component.ts
+++ src/app/features/try/try.component.ts
@@ -1,54 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {  expandElement, slideUpDown } from '../../core/services/animation/signup_animation.service';
+import { GoogleSigninService } from '../../core/services/google-signin.service';
 
 @Component({
   selector: 'app-try',
   templateUrl: './try.component.html',
   styleUrl: './try.component.css',
   animations: [
-    trigger('expandElement', [
-      state('void', style({
-        height: '0',
-        marginTop: '0',
-        opacity: '0',
-        transform: 'translateY(-20px)'
-      })),
-      transition(':enter', [
-        animate('0.5s ease-out', style({
-          height: '*',
-          marginTop: '*',
-          opacity: '1',
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('slideUpDown', [
-      state('down', style({
-        transform: 'translateY(100%)',
-        display: 'none'
-      })),
-      state('up', style({
-        transform: 'translateY(0)',
-        display: 'block'
-      })),
-      transition('down => up', [
-        style({ display: 'block' }),
-        animate('300ms ease-in-out')
-      ]),
-      transition('up => down', [
-        animate('300ms ease-in-out')
-      ])
-    ])
+    expandElement,
+    slideUpDown
   ]
 })
 export class TryComponent implements OnInit {
   isLoginVisible = false;
 
-  toggleLogin() {
-    this.isLoginVisible = !this.isLoginVisible;
-  }
+  constructor(private googleSigninService: GoogleSigninService
+  ) {}
 
   ngOnInit(): void {
-    console.log("loaded");
+    // Підключення Google SDK
+    this.googleSigninService.loadSdk();
+  }
+  toggleLogin() {
+    this.isLoginVisible = !this.isLoginVisible;
   }
 }
