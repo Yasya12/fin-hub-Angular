81b6427044f64175ab2e10d9db9c6da6e4b50739
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-03 22:17:42 +0200

    config: add provideHttpClient(withFetch()) to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,15 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations} from "@angular/platform-browser/animations";
+import { withFetch } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(), 
+    provideHttpClient(withFetch()), 
     provideAnimations()
   ]
 };
