a7f3f17772a40487d3981fd0b2e28dc34d4f0986
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-03 22:17:14 +0200

    feat: add googleLogin method to AuthService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,10 @@ import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user.model.";
 import {Observable} from "rxjs";
 
+export interface GoogleLoginResponse {
+  token: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,4 +17,9 @@ export class AuthService {
   signup(credentials: User) : Observable<object> {
     return this.http.post("http://localhost:8080/api/user", credentials)
   }
+
+  googleLogin(tokenRequest: { token: string }): Observable<GoogleLoginResponse> {
+    return this.http.post<GoogleLoginResponse>("http://localhost:8080/api/auth/google", tokenRequest);
+  }
+
 }
