492df7cd2941281f7a47c58089c1023432d78d3a
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-16 23:43:49 +0200

    feat: add hero and post sections to home component for better logic separation

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { PostsComponent } from './posts/posts.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { NgOptimizedImage } from '@angular/common'
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PostsComponent,
+    HeroSectionComponent
   ],
   imports: [
     CommonModule,
+    NgOptimizedImage,
     HomeRoutingModule
   ]
 })
