66f127a2cc7131970a31f4767fd1de400f56c25f
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-16 23:37:52 +0200

    feat: implement Post service with GET request

---
+++ src/app/core/services/post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import {Post} from "../models/post.model";
+import { PostResponse } from '../models/post_response';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<PostResponse>('http://localhost:8080/api/post').pipe(
+      map((response) => response.items) 
+    );
+  }
+}
