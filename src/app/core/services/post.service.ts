3a46b13bfc676f35c33195896bb3a83b46b6ccc8
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-18 19:56:12 +0200

    feat: add method in post service to get post by id

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import {Post} from "../models/post.model";
+import {SinglePost} from "../models/single_post.model";
 import { PostResponse } from '../models/post_response';
 
 
@@ -15,5 +16,9 @@ export class PostService {
     return this.http.get<PostResponse>('http://localhost:8080/api/post').pipe(
       map((response) => response.items) 
     );
+  }
+
+  getPostById(id: string): Observable<SinglePost> {
+    return this.http.get<SinglePost>(`${'http://localhost:8080/api/post'}/${id}`);
   }
 }
