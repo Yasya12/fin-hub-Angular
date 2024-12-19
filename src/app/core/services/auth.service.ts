5f863e2ef9c43a2de91122a53d467acf4834df22
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-19 18:43:14 +0200

    feat: implement login and logout methods, add method to fetch user and track changes using BehaviorSubject

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user.model.";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable, tap} from "rxjs";
+import { Login } from '../models/login.model';
 
 export interface GoogleLoginResponse {
   token: string;
@@ -11,11 +12,42 @@ export interface GoogleLoginResponse {
   providedIn: 'root'
 })
 export class AuthService {
+  
+  private userSubject = new BehaviorSubject<any>(this.getUser()); // Зберігаємо користувача в BehaviorSubject
+  user$ = this.userSubject.asObservable(); 
 
   constructor(private http: HttpClient) { }
 
+  getUser() {
+    if (typeof window !== 'undefined') {
+      const user = localStorage.getItem('user');
+      return user ? JSON.parse(user) : null;
+    }
+    return null;
+  }
+
   signup(credentials: User) : Observable<object> {
     return this.http.post("http://localhost:8080/api/user", credentials)
+  }
+
+  login(credentials: Login): Observable<object> {
+    return this.http.post("http://localhost:8080/api/auth/login", credentials).pipe(
+      tap((response: any) => {
+        // Перевірка наявності користувача та токену в відповіді
+        if (response && response.user && response.token) {
+          // Зберігаємо користувача та токен в localStorage
+          localStorage.setItem('user', JSON.stringify(response.user));
+          localStorage.setItem('token', response.token); // Зберігаємо токен
+          this.userSubject.next(response.user); 
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.userSubject.next(null); 
   }
 
   googleLogin(tokenRequest: { token: string }): Observable<GoogleLoginResponse> {
