2e4e88ed147c609471602c138a3cc02636f0c9da
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-19 18:49:43 +0200

    feat: display user email in header after login and add logout button

--- src/app/core/ui/header/header.component.ts
+++ src/app/core/ui/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: any = null;
+  userEmail: string | null = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+    // Перенаправляємо користувача на сторінку входу або домашню сторінку
+    window.location.href = '/home'; // або використовуйте Router
+  }
 }
