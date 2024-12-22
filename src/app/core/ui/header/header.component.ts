174334d46bad2bb81a1e810a420f9b8382e26a4d
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-22 16:19:41 +0200

    feat: update header to include dropdown menu and display user login status

--- src/app/core/ui/header/header.component.ts
+++ src/app/core/ui/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,20 +8,41 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
+  loggedIn = false;
+  isDropdownOpen = false;
   user: any = null;
   userEmail: string | null = null;
+  cdRef: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    this.authService.user$.subscribe(user => {
-      this.user = user;
-    });
+    const token = this.authService.getToken();
+  
+    if (token) {
+      this.loggedIn = true;
+      this.authService.user$.subscribe(user => {
+        this.user = user; // Оновлюємо значення user
+        if (!user) {
+          this.router.navigate(['/login']); // Перенаправлення, якщо користувач неавторизований
+        }
+      }, error => {
+        console.error('Error fetching user data', error);
+        this.authService.logout(); // Якщо сталася помилка, вийти з системи
+      });
+    } else {
+      // Якщо токен відсутній, користувач не авторизований
+      this.user = null;
+    }
   }
 
-  onLogout() {
+  toggleDropdown() {
+    console.log("Dropdown toggled: ", this.isDropdownOpen);  // Debugging log
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  logout() {
     this.authService.logout();
-    // Перенаправляємо користувача на сторінку входу або домашню сторінку
-    window.location.href = '/home'; // або використовуйте Router
+    this.router.navigate(['/home']); 
   }
 }
