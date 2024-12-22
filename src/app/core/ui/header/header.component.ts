8ee03d4eac9f186b9cea9e8116c65f4e190e798e
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-22 16:40:12 +0200

    Revert "Login display"

--- src/app/core/ui/header/header.component.ts
+++ src/app/core/ui/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,41 +7,20 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
-  loggedIn = false;
-  isDropdownOpen = false;
   user: any = null;
   userEmail: string | null = null;
-  cdRef: any;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    const token = this.authService.getToken();
-  
-    if (token) {
-      this.loggedIn = true;
-      this.authService.user$.subscribe(user => {
-        this.user = user; // Оновлюємо значення user
-        if (!user) {
-          this.router.navigate(['/login']); // Перенаправлення, якщо користувач неавторизований
-        }
-      }, error => {
-        console.error('Error fetching user data', error);
-        this.authService.logout(); // Якщо сталася помилка, вийти з системи
-      });
-    } else {
-      // Якщо токен відсутній, користувач не авторизований
-      this.user = null;
-    }
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
   }
 
-  toggleDropdown() {
-    console.log("Dropdown toggled: ", this.isDropdownOpen);  // Debugging log
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  logout() {
+  onLogout() {
     this.authService.logout();
-    this.router.navigate(['/home']); 
+    // Перенаправляємо користувача на сторінку входу або домашню сторінку
+    window.location.href = '/home'; // або використовуйте Router
   }
 }
