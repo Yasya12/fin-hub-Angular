c5a6c8103feba9c953a5a3961c72c906c2eedc11
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-28 02:07:08 +0200

    feat: update comment service by adding new methods

--- src/app/core/services/comment.service.ts
+++ src/app/core/services/comment.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import {Comment} from "../models/Comment/comment.model";
+import { Observable } from 'rxjs';
+import { CommentDisplay } from '../models/Comment/commentDisplay.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,20 @@ import {Comment} from "../models/Comment/comment.model";
 export class CommentService {
   constructor(private http: HttpClient) {}
 
-  addComment(comment: Comment){
-    return this.http.post("http://localhost:8080/api/Comment", comment)
+  addComment(comment: Comment) : Observable<Comment> {
+    return this.http.post<Comment>("http://localhost:8080/api/Comment", comment)
+  }
+
+  deleteComment(commentId: string) {
+    return this.http.delete(`http://localhost:8080/api/Comment/${commentId}`);
+  }  
+
+  getComments(postId: string, skip: number, take: number): Observable<CommentDisplay[]> {
+    const params = new HttpParams()
+      .set('postId', postId)
+      .set('skip', skip.toString())
+      .set('take', take.toString());
+
+    return this.http.get<CommentDisplay[]>(`http://localhost:8080/api/Comment`, { params });
   }
 }
