17c036704a0ac5f120b58da0fb5637a0859ad629
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2024-12-29 20:27:07 +0200

    refactor: update User model

--- src/app/core/models/user.model..ts
+++ src/app/core/models/user.model..ts
@@ -1,8 +1,7 @@
 export interface User {
   id?: string;
   email: string;
-  password: string;
   username: string;
-  country: string;
-  dateOfBirth: Date;
+  role: string;
+  profilePictureUrl: string;
 }
