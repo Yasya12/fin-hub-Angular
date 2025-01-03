b73e78960aa37c6607bb3209e6f7bc776214de54
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-01-03 15:32:06 +0200

    feat: add newly created components to the home module

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -7,13 +7,19 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { NgOptimizedImage } from '@angular/common';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { FormsModule } from '@angular/forms';
+import { CommentComponent } from './post-detail/comments/comment/comment.component';
+import { CommentFormComponent } from './post-detail/comments/comment-form/comment-form.component';
+import { CommentListComponent } from './post-detail/comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     PostsComponent,
     HeroSectionComponent,
-    PostDetailComponent
+    PostDetailComponent,
+    CommentComponent,
+    CommentListComponent,
+    CommentFormComponent
   ],
   imports: [
     CommonModule,
