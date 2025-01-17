315bb94e8b7d41d077e055df624e18be7ef93231
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-01-17 16:26:00 +0200

    feat: create LikeService for handling like functionality

---
+++ src/app/core/services/like.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { ToggleLikeResponse } from '../models/Like/toggle-like-response.model';
+import { IsLikedResponse } from '../models/Like/is-liked-response.model';
+import { environment } from '../../../../environment';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LikeService {
+    private baseUrl = environment.apiUrl;
+
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+    private getToken(): string {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            throw new Error('User is not authenticated');
+        }
+        return token;
+    }
+
+    toggleLike(postId: string): Observable<boolean> {
+        const token = this.getToken();
+
+        return this.http.post<ToggleLikeResponse>(`${this.baseUrl}/Like/toggle-like/${postId}`, {}, {
+            headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+        })
+            .pipe(
+                map((response) => response.success),
+                catchError((error) => {
+                    //  this.errorHandler.showErrorMessage('Failed to toggle like');
+                    return throwError(() => new Error('Failed to toggle like'));
+                })
+            );
+    }
+
+    isPostLiked(postId: string): Observable<boolean> {
+        const token = this.getToken();
+
+        return this.http.get<IsLikedResponse>(`${this.baseUrl}/Like/is-liked/${postId}`, {
+            headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+        })
+            .pipe(
+                map((response) => response.isLiked),
+                catchError((error) => {
+                    // this.errorHandler.showErrorMessage('Failed to check like');
+                    return throwError(() => new Error('Failed to check like'));
+                })
+            );
+    }
+}
