708c50a2e32c58676a7646f2c9a06a628771e6c4
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-01-17 16:27:33 +0200

    refactor: improve Post component for better logic and readability

--- src/app/features/home/ui/posts/posts.component.ts
+++ src/app/features/home/ui/posts/posts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../../../../core/models/Post/post.model';
 import { PostService } from "../../../../core/services/post.service";
 import { AuthService } from "../../../../core/services/auth.service";
+import { LikeService } from '../../../../core/services/like.service';
+
 
 @Component({
   selector: 'app-posts',
@@ -11,35 +13,58 @@ export class PostsComponent implements OnInit {
   posts: Post[] = [];
   loading = true;
 
-  constructor(private postService: PostService, private authService: AuthService) {}
+  constructor(
+    private postService: PostService,
+    private authService: AuthService,
+    private likeService: LikeService
+  ) { }
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe(
-      (data) => {
-        this.posts = data; 
-        this.loading = false;
-
-        const token = this.authService.currentUser()?.token;
-        if (token) {
-          this.posts.forEach(post => this.checkIfLiked(post));
-        }
-      },
-      (error) => {
-        this.loading = false;
-      }
-    );
+    this.loadPosts();
   }
 
-  checkIfLiked(post: Post): void {
-    this.postService.isPostLiked(post.id).subscribe((response: any) => {
-      post.isLiked = response.isLiked;
-    });
+  loadPosts(): void {
+    this.loading = true;
+    const userId = this.authService.currentUser()?.user.id;
+
+    if (userId) {
+      this.postService.getPostsWithLikes(userId).subscribe(
+        (data) => {
+          this.posts = data;
+          this.loading = false;
+          console.log(this.posts);
+        },
+        (error) => {
+          this.loading = false;
+          //console.error('Error loading posts with likes:', error);
+        }
+      );
+    } else {
+      this.postService.getPosts().subscribe(
+        (data) => {
+          this.posts = data;
+          this.loading = false;
+        },
+        (error) => {
+          this.loading = false;
+          //console.error('Error loading posts:', error);
+        }
+      );
+    }
   }
 
   toggleLike(post: Post): void {
-    this.postService.toggleLike(post.id).subscribe((response: any) => {
-      post.isLiked = !post.isLiked;
-      post.likesCount += post.isLiked ? 1 : -1;
+    this.likeService.toggleLike(post.id).subscribe(() => {
+      // Копіюємо пост у новий об'єкт
+      const updatedPost = { ...post };
+  
+      // Оновлюємо властивості нового об'єкта
+      updatedPost.isLiked = !post.isLiked;
+      updatedPost.likesCount += updatedPost.isLiked ? 1 : -1;
+  
+      // Замінюємо старий пост у масиві на оновлений
+      this.posts = this.posts.map(p => p.id === post.id ? updatedPost : p);
     });
   }
+  
 }
