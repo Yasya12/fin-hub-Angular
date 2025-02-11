90f65b5f94818f863bb931bc34fe3b88da0be820
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-02-11 17:01:10 +0200

    update app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,34 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/layouts/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './layouts/layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
   {
-    path: 'signup',
-    loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule)
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'signup',
+        loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule)
+      }
+    ]
   },
   {
-    path: 'try',
-    loadChildren: () => import('./features/try/try.module').then(m => m.TryModule)
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'try',
+        loadChildren: () => import('./features/try/try.module').then(m => m.TryModule)
+      },
+      {
+        path: 'post-detail/:id',
+        loadChildren: () => import('./features/post-detail/post-detail.module').then(m => m.PostDetailModule)
+      }
+    ]
   },
-  {
-    path: 'home',
-    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'post-detail/:id',
-    loadChildren: () => import('./features/post-detail/post-detail.module').then(m => m.PostDetailModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
-    path: '', redirectTo: 'home', pathMatch: 'full'
-  }
 ];
