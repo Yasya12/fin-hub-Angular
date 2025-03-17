b0e469885bf8a9f6033838a301e124aa548fa9ba
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-03-17 19:49:54 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { AuthService } from './core/services/auth.service';
+import { ScrollService } from './shared/services/scroll.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,22 @@ import { AuthService } from './core/services/auth.service';
   imports: [RouterOutlet, CoreModule],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  // Services
+  private readonly authService = inject(AuthService);
+  private readonly scrollService = inject(ScrollService);
+
+  //States
   title = 'fin-hub';
 
-  constructor(private authService: AuthService) {}
+  // HtmlElements
+  scrollContainer = viewChild<ElementRef>('scrollContainer');
 
   ngOnInit(): void {
     this.authService.checkTokenExpiration();
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollService.setScrollContainer(this.scrollContainer()!);
   }
 }
