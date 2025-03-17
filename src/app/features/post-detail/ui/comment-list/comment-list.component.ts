190bbf0b0e6f38d4c4f09c1dd43d9a8f3a2ac928
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-03-17 20:34:25 +0200

    update comment list component to have pagination on scroll and fix the showing of the comments

--- src/app/features/post-detail/ui/comment-list/comment-list.component.ts
+++ src/app/features/post-detail/ui/comment-list/comment-list.component.ts
@@ -1,72 +1,69 @@
-import { Component, Input, signal, Output, EventEmitter } from '@angular/core';
-import { Comment } from '../../../../core/models/Comment/comment.model';
-import { CommentDisplay } from '../../../../core/models/Comment/commentDisplay.model';
+import { Component, signal, Output, EventEmitter, inject, input, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment.model';
+import { CommentDisplay } from '../../models/commentDisplay.model';
 import { CommentService } from '../../services/comment.service';
 import { AuthService } from '../../../../core/services/auth.service';
 import { ResponseModel } from '../../../signup/models/response.model';
+import { ScrollService } from '../../../../shared/services/scroll.service';
 
 @Component({
   selector: 'app-comment-list',
   templateUrl: './comment-list.component.html',
 })
-export class CommentListComponent {
-  @Input() postId!: string;
+export class CommentListComponent implements OnInit {
+  // Services
+  private readonly authService = inject(AuthService);
+  private readonly commentService = inject(CommentService);
+  private readonly scrollService = inject(ScrollService);
+
+  // Inputs
+  postId = input<string>();
+
+  // States
+  currentUser = signal<ResponseModel | undefined>(undefined);
+  comments = signal<CommentDisplay[]>([]);
+  selectedCommentId: string | null = null;
+  pageNumber = 1;
+  pageSize = 10;
+  hasMoreComments = true;
+  lastScrollTop: number = 0;
+
   @Output() commentsUpdated = new EventEmitter<number>();
 
-  currentUser = signal<ResponseModel | undefined>(undefined);
-
-  allComments = signal<CommentDisplay[]>([]);
-  comments = signal<CommentDisplay[]>([]);
-
-  selectedCommentId: string | null = null;
-
-  currentPage: number = 0;
-  pageSize: number = 3;
-  hasMoreComments: boolean = true;
-
-
-  constructor(private authService: AuthService, private commentService: CommentService) {
+   // Lifecycle hooks
+   ngOnInit(): void {
     this.currentUser.set(this.authService.currentUser());
-  }
-
-  ngOnInit(): void {
     this.loadComments();
+    this.scrollService.scrollContainer$.subscribe(container => {
+      if (container) {
+        container.nativeElement.addEventListener('scroll', this.onScroll.bind(this));
+      }
+    });
   }
 
+  //Methods
   private loadComments(): void {
-    this.commentService.getComments(this.postId).subscribe(
-      (commentsDisplay: CommentDisplay[]) => {
-        this.processComments(commentsDisplay);
-        this.comments.set(this.getSortedComments());
-        this.updatePaginatedComments();
-        setTimeout(() => {
-          this.commentsUpdated.emit(this.comments().length);
-        });
+    this.commentService.getComments(this.postId()!, this.pageNumber, this.pageSize).subscribe(
+      (items) => {
+        if (items.length < this.pageSize) {
+          this.hasMoreComments = false;
+        }
+        const processedComments = this.processComments(items);
+        this.getSortedComments();
+        this.comments.set([...this.comments(), ...processedComments]);
+        this.pageNumber++;
       },
       (error) => console.error('Error fetching comments:', error)
     );
   }
 
-  updatePaginatedComments(): void {
-    const sorted = this.getSortedComments();
-    this.allComments.set(this.comments().slice(0, this.pageSize));
-    this.hasMoreComments = sorted.length > this.pageSize;
-  }
-
-  loadMoreComments(): void {
-    this.pageSize += 3;
-    this.updatePaginatedComments();
-  }
-
-  private processComments(commentsDisplay: CommentDisplay[]): void {
-    const processedComments = commentsDisplay.map(comment => ({
+  processComments = (comments: CommentDisplay[]): CommentDisplay[] => {
+    return comments.map(comment => ({
       ...comment,
-      createdAt: new Date(comment.createdAt)
+      createdAt: new Date(comment.createdAt),
+      replies: comment.replies ? this.processComments(comment.replies) : [] // Process replies recursively
     }));
-
-    const tree = this.buildCommentTree(processedComments);
-    this.comments.set(tree);
-  }
+  };
 
   getSortedComments(): CommentDisplay[] {
     return [...this.comments()]
@@ -76,27 +73,17 @@ export class CommentListComponent {
 
   updateComments(filtered: CommentDisplay[]): void {
     this.comments.set(filtered);
-    this.allComments.set(filtered.slice(0, this.pageSize)); // Оновлюємо відображені коментарі
-    this.hasMoreComments = filtered.length > this.pageSize;
-  }
-
-
-  getReplies(parentId: string): CommentDisplay[] {
-    return this.comments().filter(comment => comment.parentId === parentId)
-      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
   }
 
   addComment(newComment: Comment): void {
     newComment.authorId = this.currentUser()?.user.id || '';
+
     this.commentService.addComment(newComment).subscribe({
       next: (response: Comment) => {
         const transformedResponse = this.mapToCommentDisplay(response);
-
         this.comments.set([...this.comments(), transformedResponse]);
         this.comments.set(this.getSortedComments());
-        this.commentsUpdated.emit(this.comments().length);
-
-        this.allComments.set(this.comments().slice(0, this.pageSize));
+        this.commentsUpdated.emit();
       },
       error: (err) => console.error('Error adding comment:', err),
     });
@@ -104,17 +91,22 @@ export class CommentListComponent {
 
   addReply(newReply: Comment): void {
     newReply.authorId = this.currentUser()?.user.id || '';
+
     this.commentService.addComment(newReply).subscribe({
       next: (response: Comment) => {
         const transformedResponse = this.mapToCommentDisplay(response);
-
         if (transformedResponse.parentId) {
           const parent = this.findCommentById(transformedResponse.parentId);
+          
           if (parent) {
-            parent.children = [...(parent.children || []), transformedResponse];
+            parent.replies = [...(parent.replies || []), transformedResponse];
+            this.comments.update(existingComments => [...existingComments]);
           }
-          this.loadComments();
+        } else {
+          this.comments.update(existingComments => [...existingComments, transformedResponse]);
         }
+
+        this.commentsUpdated.emit(this.comments().length);
       },
       error: (err) => console.error('Error adding reply:', err),
     });
@@ -125,19 +117,26 @@ export class CommentListComponent {
     while (stack.length) {
       const current = stack.pop();
       if (current?.id === id) return current;
-      if (current?.children?.length) stack.push(...current.children);
+      if (current?.replies?.length) stack.push(...current.replies);
     }
     return undefined;
   }
 
-
   deleteComment(commentId: string): void {
     this.commentService.deleteComment(commentId).subscribe({
       next: () => {
-        this.loadComments();
+        this.comments.update(comments => this.removeCommentFromTree(comments, commentId));
       },
       error: (err) => console.error('Error deleting comment:', err),
     });
+  }
+
+  private removeCommentFromTree(comments: CommentDisplay[], commentId: string): CommentDisplay[] {
+    return comments
+      .filter(comment => comment.id !== commentId)
+      .map(comment => ({
+        ...comment,
+      }));
   }
 
   private mapToCommentDisplay(comment: Comment): CommentDisplay {
@@ -152,40 +151,23 @@ export class CommentListComponent {
     return transformed;
   }
 
-  private buildCommentTree(comments: CommentDisplay[]): CommentDisplay[] {
-    const map = new Map<string, CommentDisplay>();
-
-    comments.forEach(comment => {
-      comment.children = [];
-      map.set(comment.id, comment);
-    });
-
-    const rootComments: CommentDisplay[] = [];
-
-    comments.forEach(comment => {
-      if (comment.parentId) {
-        const parent = map.get(comment.parentId);
-        if (parent) {
-          parent.children?.push(comment);
-        }
-      } else {
-        rootComments.push(comment);
-      }
-    });
-
-    return rootComments;
-  }
-
   onChildToggleMenu(newSelectedId: string | null): void {
     this.selectedCommentId = newSelectedId;
   }
-
+  
   isNewComment(comment: CommentDisplay): boolean {
     const now = new Date();
     const commentDate = new Date(comment.createdAt);
     const diffInMinutes = (now.getTime() - commentDate.getTime()) / 60000;
-    return diffInMinutes <= 1; // Вважаємо комент новим, якщо йому менше 5 хвилин
+    return diffInMinutes <= 1;
   }
 
-
+  // Event handlers
+  onScroll(event: Event): void {
+    if (!this.hasMoreComments) return;
+    const container = (event.target as HTMLElement);
+    if (container.scrollTop + container.clientHeight >= container.scrollHeight - container.scrollHeight * 0.1) {
+      this.loadComments();
+    }
+  }
 }
